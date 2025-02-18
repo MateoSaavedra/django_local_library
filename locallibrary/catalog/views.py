347e9a43641bf64ace3f3bc6d7f1f709dfa1def0
Mateo Saavedra <mateosaavedra838@gmail.com>
2025-02-18 02:56:00 -0500

    part 5 challenge done

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -3,6 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 from .models import Book, Author, BookInstance, Genre
+from django.db.models import Q
+
 
 def index(request):
     """View function for home page of site."""
@@ -17,11 +19,21 @@ def index(request):
     # The 'all()' is implied by default.
     num_authors = Author.objects.count()
 
+    # Count total genres
+    num_genres = Genre.objects.count()
+
+    # Count books containing a specific word (e.g., "adventure", case insensitive)
+    search_word = "harry"
+    num_books_with_word = Book.objects.filter(title__icontains=search_word).count()
+
     context = {
         'num_books': num_books,
         'num_instances': num_instances,
         'num_instances_available': num_instances_available,
         'num_authors': num_authors,
+        'num_genres': num_genres,
+        'num_books_with_word': num_books_with_word,
+        'search_word': search_word,
     }
 
     # Render the HTML template index.html with the data in the context variable
